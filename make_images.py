b8d7d74c3b169d8b72e9ea09c1b925088824dd47
Darrel Belvin <darrel.belvin@gmail.com>
2020-02-07 23:26:47 +0000

    full image aws half batch

--- make_images.py
+++ make_images.py
@@ -1,5 +1,5 @@
 from dc_tests import test_building
 
 for i in range(1,10):
-    test_building(i, score_all=True, save_prefix = f'figures/bldg-{i}')
+    test_building(i, None, score_all=True, save_prefix = f'figures/bldg-{i}')
     print(f"completed {i}")
